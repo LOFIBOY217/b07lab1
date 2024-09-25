869dbe30d757efa3f0f7953b29237773fda11911
LOFIBOY217 <jiaaqii.huang@mail.utoronto.ca>
2024-09-24 21:54:45 -0400

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,18 +1,21 @@
-package Driver;
+package lab2;
 
 public class Driver {
-	public static void main(String [] args) {
-		Polynomial p = new Polynomial();
-		System.out.println(p.evaluate(3));
-		double [] c1 = {6,0,0,5};
-		Polynomial p1 = new Polynomial(c1);
-		double [] c2 = {0,-2,0,0,-9};
-		Polynomial p2 = new Polynomial(c2); 
-		Polynomial s = p1.Add(p2);
-		System.out.println("s(0.1) = " + s.evaluate(0.1)); 
-		if(s.hasRoot(1))
-			System.out.println("1 is a root of s");
-		else
-			System.out.println("1 is not a root of s");
-	}
+    public static void main(String[] args) {
+        Polynomial p = new Polynomial();
+        System.out.println(p.evaluate(3));
+        
+        double[] c1 = {6, 0, 0, 5};
+        Polynomial p1 = new Polynomial(c1, new int[]{0, 1, 2, 3});  
+        double[] c2 = {0, -2, 0, 0, -9};
+        Polynomial p2 = new Polynomial(c2, new int[]{0, 1, 2, 3, 4});  
+
+        Polynomial s = p1.add(p2);
+        System.out.println("s(0.1) = " + s.evaluate(0.1));
+
+        if (s.hasRoot(1))
+            System.out.println("1 is a root of s");
+        else
+            System.out.println("1 is not a root of s");
+    }
 }
