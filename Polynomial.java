f25dc4c6914402c0ea26f1097e0a89d38c1846f8
LOFIBOY217 <jiaaqii.huang@mail.utoronto.ca>
2024-09-29 00:30:46 -0400

    Update Polynomial.java

--- Polynomial.java
+++ Polynomial.java
@@ -1,166 +1,161 @@
 package lab2;
 
 import java.io.File;
-import java.io.FileWriter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.TreeMap;
 import java.util.Scanner;
-import java.io.IOException;
 
-public class Polynomial {
-    public double[] coefficients;
-    public int[] exponents;
+public class Polynomials {
+    int[] exponents;
+    double[] coefficients;
 
-    public Polynomial() {
-        coefficients = new double[1];
-        coefficients[0] = 0;
-        exponents = new int[1];
-        exponents[0] = 0;
+    public Polynomials(){
+        this.coefficients = new double[1];
+        this.exponents = new int[1];
+        coefficients[0] = 1;
+        exponents[0] = 1;
     }
 
-    public Polynomial(double[] co, int[] expo) {
-        this.coefficients = co.clone();
-        this.exponents = expo.clone();
-    }
-    
-    public Polynomial(File file) throws IOException{
-    	Scanner scan = new Scanner(file);
-    	String po = scan.nextLine();
-    	scan.close();
-    	
-    	String[] terms = po.split("(?=[+-])");
-    	
-    	int[] expo = new int[terms.length];
-    	double[] co = new double[terms.length];
-    	
-    	for (int i = 0; i < terms.length; i++) {
-    		if (terms[i].contains("x")) {
-    			expo[i] = 0;
-    			co[i] = Integer.parseInt(terms[i]);
-    		}
-    		else {
-    			String[] co_expo = terms[i].split("x");
-
-    			if (co_expo[0].isEmpty()) {
-    			    co[i] = 1.0;
-    			} else if (co_expo[0].equals("-")) {
-    			    co[i] = -1.0;
-    			} else {
-    			    co[i] = Double.parseDouble(co_expo[0]);
-    			}
-
-    			if (co_expo.length > 1 && !co_expo[1].isEmpty()) {
-    			    expo[i] = Integer.parseInt(co_expo[1]);
-    			} else {
-    			    expo[i] = 1; 
-    			}
-    		}
-    	}
-    	
-    	this.coefficients = co.clone();
-        this.exponents = expo.clone();
+    public Polynomials(int[] exponents, double[] coefficients){
+        this.coefficients = coefficients.clone();
+        this.exponents = exponents.clone();
     }
 
-    public HashMap<Integer, Double> arrToMap() {
-        HashMap<Integer, Double> dic = new HashMap<>();
-        for (int i = 0; i < coefficients.length; i++) {
-            dic.put(exponents[i], coefficients[i]);
+    public Polynomials(File file) throws FileNotFoundException {
+        Scanner fileScanner = new Scanner(file);
+        String target = fileScanner.nextLine();
+        String[] targetArray = target.split("(?=[+-])");
+        HashMap<Integer, Double> resultMap = new HashMap<>();
+        for (String expression: targetArray) {
+            if (expression.indexOf("x") != -1) {
+                int indexX = expression.indexOf("x");
+
+                Integer expo = (indexX + 1 < expression.length()) ?
+                        Integer.parseInt(expression.substring(indexX + 1)) : 1;
+
+                String coefficientStr = expression.substring(0, indexX);
+                Double co;
+
+                if (coefficientStr.equals("") || coefficientStr.equals("+")) {
+                    co = 1.0;
+                } else if (coefficientStr.equals("-")) {
+                    co = -1.0;
+                } else {
+                    co = Double.parseDouble(coefficientStr);
+                }
+
+                resultMap.put(expo, co);
+            }
+            else {
+                resultMap.put(0, Double.parseDouble(expression));
+            }
         }
-        return dic;
-    }
-
-    public Polynomial add(Polynomial po) {
-        HashMap<Integer, Double> thisPo = arrToMap();
-        HashMap<Integer, Double> newPo = po.arrToMap();
-
-        for (Map.Entry<Integer, Double> entry : newPo.entrySet()) {
-            thisPo.put(entry.getKey(), thisPo.getOrDefault(entry.getKey(), 0.0) + entry.getValue());
-        }
-
-        int[] expo = new int[thisPo.size()];
-        double[] co = new double[thisPo.size()];
-
-        TreeMap<Integer, Double> sortedDic = new TreeMap<>(thisPo);
-
+        int[] resultExponents = new int[resultMap.size()];
+        double[] resultCoefficients = new double[resultMap.size()];
         int i = 0;
-        for (Map.Entry<Integer, Double> entry : sortedDic.entrySet()) {
-            expo[i] = entry.getKey();
-            co[i] = entry.getValue();
+        for (Map.Entry<Integer, Double> entry: resultMap.entrySet()){
+            resultExponents[i] = entry.getKey();
+            resultCoefficients[i] = entry.getValue();
             i++;
         }
 
-        return new Polynomial(co, expo);
+        this.coefficients = resultCoefficients.clone();
+        this.exponents = resultExponents.clone();
     }
 
-    public double evaluate(double arg) {
+    public Polynomials mapToPolynomial(HashMap<Integer, Double> map){
+        int[] exponents = new int[map.size()];
+        double[] coefficients = new double[map.size()];
+        int i = 0;
+        for (Map.Entry<Integer, Double> entry : map.entrySet()){
+            exponents[i] = entry.getKey();
+            coefficients[i] = entry.getValue();
+            i++;
+        }
+        return new Polynomials(exponents, coefficients);
+    }
+
+
+
+    public Polynomials add(Polynomials newPolynomials){
+        HashMap<Integer, Double> resultMap = new HashMap<>();
+
+        for (int i = 0; i < this.exponents.length; i++){
+            resultMap.put(this.exponents[i], this.coefficients[i]);
+        }
+
+        for (int i = 0; i < newPolynomials.exponents.length; i++){
+            if (resultMap.containsKey(newPolynomials.exponents[i])){
+                double currValue = resultMap.get(newPolynomials.exponents[i]);
+                resultMap.put(newPolynomials.exponents[i], currValue + newPolynomials.coefficients[i]);
+            }
+            else{
+                resultMap.put(newPolynomials.exponents[i], newPolynomials.coefficients[i]);
+            }
+        }
+        return mapToPolynomial(resultMap);
+    }
+
+    public double evaluate(double arg){
         double result = 0;
-        for (int i = 0; i < coefficients.length; i++) {
-            result += coefficients[i] * Math.pow(arg, exponents[i]);
+        for (int i = 0; i < this.exponents.length; i++){
+            result += this.coefficients[i] * Math.pow(arg, this.exponents[i]);
         }
         return result;
     }
 
-    public boolean hasRoot(double arg) {
-        return evaluate(arg) == 0;
+    public boolean hasRoot(double arg){
+        return (this.evaluate(arg) == 0);
     }
-    
-    public Polynomial multiply(Polynomial po) {
-    	HashMap<Integer, Double> thisPo = arrToMap();
-        HashMap<Integer, Double> newPo = po.arrToMap();
-        
-        HashMap<Integer, Double> rePo = new HashMap<>();
-        
-        for (Map.Entry<Integer, Double> entry1 : newPo.entrySet()) {
-        	for (Map.Entry<Integer, Double> entry2 : thisPo.entrySet()) {
-        		int new_expo = entry1.getKey() + entry2.getKey();
-        		double new_co = entry1.getValue() * entry2.getValue();;
-        		rePo.put(new_expo, new_co + rePo.getOrDefault(new_expo, 0.0));
-        	}
+
+    public Polynomials multiply(Polynomials newPolynomial){
+        HashMap<Integer, Double> resultMap = new HashMap<>();
+        for (int i = 0; i < this.exponents.length; i++){
+            for (int j = 0; j < newPolynomial.exponents.length; j++){
+                int currExponent = this.exponents[i] + newPolynomial.exponents[j];
+                if (resultMap.containsKey(currExponent)){
+                    double currCoefficient = resultMap.get(currExponent);
+                    resultMap.put(currExponent, currCoefficient + this.coefficients[i] * newPolynomial.coefficients[j]);
+                }
+                else{
+                    resultMap.put(currExponent, this.coefficients[i] * newPolynomial.coefficients[j]);
+                }
+            }
         }
-        
-        int[] expo = new int[rePo.size()];
-        double[] co = new double[rePo.size()];
-        int i = 0;
-        
-        for (Map.Entry<Integer, Double> entry : rePo.entrySet()) {
-        	expo[i] = entry.getKey();
-        	co[i] = entry.getValue();
-        	i++;
-        }
-        
-        return new Polynomial(co, expo);
+        return mapToPolynomial(resultMap);
     }
-    
-    public void saveToFile(String filename) throws IOException{
-    	try (FileWriter writer = new FileWriter(filename)){
-    		StringBuilder poly_str = new StringBuilder();
-    		
-    		poly_str.append(this.coefficients[0]);
-    		
-    		if (this.exponents[0] != 0) {
-    			poly_str.append("x");
-    			poly_str.append(this.exponents[0]);
-    		}
-    		
-    		for (int i = 1; i < this.coefficients.length; i++) {
-    			if (this.coefficients[i] > 0) {
-    				poly_str.append("+");
-    			}
-    			else {
-    				poly_str.append("-");
-    			}
-    			poly_str.append(Math.abs(this.coefficients[i]));
-    			poly_str.append("x");
-    			if (this.exponents[i] != 0) {
-    				poly_str.append(this.exponents[i]);
-    			}					
-    		}
-    		writer.write(poly_str.toString());
-    	}
-    	catch (IOException e) {
+
+    public String polynomialToString() {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < this.exponents.length; i++) {
+            if (this.coefficients[i] != 0) {
+                if (i > 0 && this.coefficients[i] > 0) {
+                    result.append("+");
+                }
+                result.append(this.coefficients[i]);
+                result.append("x");
+                result.append(this.exponents[i]);
+            }
+        }
+
+        return result.toString();
+    }
+
+    public void saveToFile(String filename) throws FileNotFoundException {
+        File file = new File(filename);
+        try (PrintWriter writer = new PrintWriter(file)) {
+            String polynomialString = polynomialToString();
+            if (polynomialString.isEmpty()) {
+                writer.println("0");
+            } else {
+                writer.println(polynomialString);
+            }
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    
 }
